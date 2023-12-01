0e88dd12a4a922322cc32ed7e997e3ac65ec26df
jmvillafanamdoza <144737939+jmvillafanamdoza@users.noreply.github.com>
2023-12-01 17:30:21 -0500

    Crud App

--- app/src/main/java/com/example/lastore/SigninActivity.kt
+++ app/src/main/java/com/example/lastore/SigninActivity.kt
@@ -32,7 +32,7 @@ class SigninActivity: AppCompatActivity() {
 
                     firebaseAuth.signInWithEmailAndPassword(email , pass).addOnCompleteListener() {
                         if (it.isSuccessful){
-                            val intent = Intent(this, SigninActivity::class.java)
+                            val intent = Intent(this, MainActivity::class.java)
                             startActivity(intent)
                         }else{
                             Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
---
+++ app/src/main/java/com/example/lastore/UserData.kt
@@ -0,0 +1,3 @@
+package com.example.lastore
+
+data class UserData(val name : String? = null, val operator : String? = null, val location : String? = null, val phone : String? = null)
